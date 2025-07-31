19f7571de69b68e29fbcd97c7145e3d9d9b58d2a
Danna Barrios <dannabarrios0820@gmail.com>
2025-07-31 00:27:44 -0500

    Todas las opciones en main

--- main.java
+++ main.java
@@ -8,8 +8,15 @@ public class main {
         System.out.println("Bienvenido al sistema de gestión de ambientes");
         System.out.println("Seleccione una opción:");
         System.out.println("1. Registrar ubicación");
-        System.out.println("2. Otra función");
-        System.out.println("3. Otra función");
+        System.out.println("2. Registrar linea de formación");
+        System.out.println("3. Registar programa de formación");
+        System.out.println("4. Registar ambiente");
+        System.out.println("5. Registrar instructor");
+        System.out.println("6. Registrar aprendiz");
+        System.out.println("7. Asignar horario");
+        System.out.println("8. Reportes");
+        System.out.println("9. Validar restricciones");
+        System.out.println("10. Salir");
         System.out.print("Opción: ");
         int opcion = scanner.nextInt();
 
